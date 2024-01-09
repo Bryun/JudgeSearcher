179f212baa380ef36ef1271c91c78033ef12b530
Bryun Msimango <bryunm@gmail.com>
2024-01-09 23:20:35 +0200

    Circuit 19 changes...

--- JudgeSearcher/Circuits/Nineteenth.cs
+++ JudgeSearcher/Circuits/Nineteenth.cs
@@ -3,6 +3,8 @@ using JudgeSearcher.Models;
 using JudgeSearcher.Utility;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using Serilog;
+using Serilog.Core;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -40,97 +42,98 @@ namespace JudgeSearcher.Circuits
 
             _ = Scraper.Scan(URL, (driver, wait) =>
             {
-                Actions action = new Actions(driver);
-
-                var counties = new string[] { "Indian River", "Martin", "Okeechobee", "Saint Lucie" };
-
-                foreach (var county in counties)
+                try
                 {
+                    Actions action = new Actions(driver);
 
-                    var primary = driver.FindElement(By.XPath("//a[contains(text(), 'Judges ')]"));
+                    var counties = new string[] { "Indian River", "Martin", "Okeechobee", "Saint Lucie" };
 
-                    action.MoveToElement(primary).Click().Perform();
-
-                    var secondary = driver.FindElement(By.XPath(string.Format("//a[contains(text(), '{0} County Judges')]", county)));
-
-                    action.MoveToElement(secondary).Click().Perform();
-
-                    foreach (var card in driver.FindElements(By.XPath("//div[@class='views-field views-field-nothing']")))
+                    foreach (var county in counties)
                     {
-                        var temp = Regex.Split(card.Text, ",|(\r\n)");
 
-                        Judge judge = new Judge()
+                        var primary = driver.FindElement(By.XPath("//a[contains(text(), 'Judges ')]"));
+
+                        action.MoveToElement(primary).Click().Perform();
+
+                        var secondary = driver.FindElement(By.XPath(string.Format("//a[contains(text(), '{0} County Judges')]", county)));
+
+                        action.MoveToElement(secondary).Click().Perform();
+
+                        foreach (var card in driver.FindElements(By.XPath("//div[@class='views-field views-field-nothing']")))
                         {
-                            Circuit = Alias,
-                            LastName = temp[0],
-                            FirstName = temp[1],
-                            County = county
-                        };
+                            var temp = Regex.Split(card.Text, ",|(\r\n)");
 
-                        card.Click();
-
-                        wait.Until((e) => By.XPath("//article[@role='article']"));
-
-                        HtmlWeb web = new HtmlWeb();
-                        var document = web.Load(driver.Url);
-
-                        var article = document.DocumentNode.SelectSingleNode("//article[@role='article']");
-
-                        var lines = article.ChildNodes[1].ChildNodes.Where((e) => e.Name == "div").ToList();
-
-                        for (int i = 1; i < lines.Count(); i++)
-                        {
-                            var t = lines[i].InnerText.Split("\n", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
-                            switch (t[0].Trim())
+                            Judge judge = new Judge()
                             {
-                                case "Position":
-                                    judge.Type = t[1].Trim();
-                                    break;
-                                case "Office Address":
-                                    judge.Street = t[1];
-                                    var array = t[1].Address();
+                                Circuit = Alias,
+                                LastName = temp[0],
+                                FirstName = temp[1],
+                                County = county
+                            };
 
-                                    switch (array.Count())
-                                    {
-                                        case 2:
-                                            //judge.Street = array[0];
-                                            judge.Zip = array[1];
-                                            break;
-                                        case 3:
-                                            //judge.Street = array[0];
-                                            judge.City = array[1];
-                                            judge.Zip = array[2];
-                                            break;
-                                        case 4:
-                                            //judge.Street = array[0];
-                                            judge.City = array[2];
-                                            judge.Zip = array[3];
-                                            break;
-                                        default:
-                                            judge.Street = t[1];
-                                            break;
-                                    }
+                            card.Click();
 
-                                    break;
-                                case "Phone":
-                                    judge.Phone = t[1].Trim();
-                                    break;
-                                case "Judicial Assistant":
-                                    judge.JudicialAssistant = t[1].Trim();
-                                    break;
-                                case "Division(s)":
-                                    judge.SubDivision = string.Join(", ", t.Skip(1));
-                                    break;
-                                default:
-                                    break;
+                            wait.Until((e) => By.XPath("//article[@role='article']"));
+
+                            HtmlWeb web = new HtmlWeb();
+                            var document = web.Load(driver.Url);
+
+                            var article = document.DocumentNode.SelectSingleNode("//article[@role='article']");
+
+                            var lines = article.ChildNodes[1].ChildNodes.Where((e) => e.Name == "div").ToList();
+
+                            for (int i = 1; i < lines.Count(); i++)
+                            {
+                                var t = lines[i].InnerText.Split("\n", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                                switch (t[0].Trim())
+                                {
+                                    case "Position":
+                                        judge.Type = t[1].Trim();
+                                        break;
+                                    case "Office Address":
+
+                                        Display(t[1]);
+
+                                        var location = Regex.Match(t[1], "Building B, Suite \\d+|Suite\\s([A-Z]\\d+|\\d+)|\\d+ Courthouse Addition|Martin County Courthouse").Value;
+
+                                        judge.Location = location;
+
+                                        judge.Street = Regex.Match(t[1], ".+Boulevard|.+Blvd\\.|.+Street\\.|.+Street|.+St\\.|.+Drive\\.|.+Drive|.+Dr\\.|.+Avenue\\.|.+Avenue").Value;
+                                        judge.Street = string.IsNullOrEmpty(location) ? judge.Street : judge.Street.Replace(location, string.Empty);
+                                        judge.Street = Regex.Replace(judge.Street, ",|\\.", string.Empty).Trim();
+
+                                        judge.Zip = Regex.Match(t[1], "((Fl|FL)\\s(\\d+))").Groups[3].Value;
+
+                                        string _zip = Regex.Match(t[1], "(,\\s(Fl|FL)\\s\\d+)").Value;
+
+                                        judge.City = t[1].Replace(judge.Street, string.Empty).Replace(_zip, string.Empty);
+                                        judge.City = string.IsNullOrEmpty(location) ? judge.City : judge.City.Replace(location, string.Empty);
+                                        judge.City = Regex.Replace(judge.City, ",|\\.", string.Empty).Trim();
+                                        break;
+                                    case "Phone":
+                                        judge.Phone = t[1].Trim();
+                                        break;
+                                    case "Judicial Assistant":
+                                        judge.JudicialAssistant = t[1].Trim();
+                                        break;
+                                    case "Division(s)":
+                                        judge.SubDivision = string.Join(", ", t.Skip(1));
+                                        break;
+                                    default:
+                                        break;
+                                }
                             }
+
+                            collection.Add(judge);
+
+                            driver.Navigate().Back();
                         }
-
-                        collection.Add(judge);
-
-                        driver.Navigate().Back();
                     }
+                }
+                catch(Exception ex)
+                {
+                    Log.Logger.Error(ex.Message);
                 }
             });
 
